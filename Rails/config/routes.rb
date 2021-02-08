72ef95bceef2c32c8eb3607c04ebe76219924852
Jerebern_VM_DEBIAN <jere.bern@hotmail.com>
2021-02-08 13:35:09 -0500

    device et tuto de mathieu fait et fonctionel

--- Rails/config/routes.rb
+++ Rails/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  root to: "angular#index"
+  namespace :api, constraints: { format: 'json' }  do
+    resources :todos
+  end
+
+  match '*url', to: "angular#index", via: :get # le parametre url contiendra tout ce qui suit l'étoile dans l'url
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
