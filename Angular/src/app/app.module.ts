0357c0d0381fa00920afa8d0ffe42a33ee377dae
Jerebern_VM_DEBIAN <jere.bern@hotmail.com>
2021-02-09 10:50:52 -0500

    test retirés & signup généré

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component'
-import { LoginComponent } from './components/login/login.component'
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
