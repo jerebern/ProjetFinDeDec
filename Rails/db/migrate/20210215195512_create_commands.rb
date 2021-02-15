a4a4b5303748e40bc173a88d9bd1e9e6be58c0a9
Jerebern_VM_DEBIAN <jere.bern@hotmail.com>
2021-02-15 16:53:37 -0500

    debut API command

---
+++ Rails/db/migrate/20210215195512_create_commands.rb
@@ -0,0 +1,15 @@
+class CreateCommands < ActiveRecord::Migration[6.0]
+  def change
+    create_table :Commands do |t|
+      t.decimal :sub_total, precision: 8, scale: 2
+      t.decimal :tps, precision: 8, scale: 2
+      t.decimal :tvq, precision: 8, scale: 2
+      t.decimal :total, precision: 8, scale: 2
+      t.boolean :store_pickup, null: false
+      t.string :state, :limit => 50, null: false
+      t.string :shipping_adress, :limit => 50, null: false
+      t.string :user_email, :limit => 50, null:false
+      
+      t.timestamps
+    end
+end
