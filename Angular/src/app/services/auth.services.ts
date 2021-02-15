8564542802ff75915a1772fc968843e3523ceaca
Jérémy Veillette <j.veillette@hotmail.ca>
2021-02-15 14:14:39 -0500

    user.models.ts

--- Angular/src/app/services/auth.services.ts
+++ Angular/src/app/services/auth.services.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map, catchError, tap, retry } from 'rxjs/operators';
-import { User } from '../models/user';
+import { User } from '../models/user.models';
 @Injectable({
   providedIn: 'root'
 })
---
+++ Rails/app/controllers/api/products_controller.rb
@@ -0,0 +1,25 @@
+class Api::ProductsController < ApplicationController
+    def index
+        @products = Product.all
+    end
+    
+    def show
+        @product = Product.find(params[:id])
+    end
+    
+    def update
+        @product = Product.find(params[:id])
+        if @article.update(product_params)
+            render json: show, status: :ok, location: @product 
+        else
+            render json: @product.errors, status: :unprocessable_entity 
+        end
+    end
+    
+    def delete
+    end
+    private
+    def product_params
+        params.require(:product).permit(:category, :price, :title, :description, :quantity, :animal_type)
+    end
+end
