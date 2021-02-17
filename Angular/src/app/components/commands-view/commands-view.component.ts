01bfd6761f271d4b733d15fce4404037ee4cd1b8
Jerebern_VM_DEBIAN <jere.bern@hotmail.com>
2021-02-17 16:11:50 -0500

    route pour les commandes :)

--- Angular/src/app/components/commands-view/commands-view.component.ts
+++ Angular/src/app/components/commands-view/commands-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Command } from 'src/app/models/command.model';
 import { ApiRequestService } from 'src/app/services/api-request.services';
 import { AuthService } from 'src/app/services/auth.services';
@@ -11,11 +12,10 @@ import { AuthService } from 'src/app/services/auth.services';
 export class CommandsViewComponent implements OnInit {
   
   currentCommand !: Command 
-  constructor(private apiRequestService : ApiRequestService, private authService:AuthService) { 
-    this.getCurrentCommandNumber();
+  constructor(private apiRequestService : ApiRequestService, private authService:AuthService, private route: ActivatedRoute) { 
   }
 
-  getCurrentCommandNumber(){
+  getCurrentCommandNumber(id : string){
     this.apiRequestService.getOneCommandFromOneUser(this.authService.currentUser,"1").subscribe(succes =>{
       if(succes){
       this.currentCommand = this.apiRequestService.getCurrentCommand();
@@ -26,6 +26,14 @@ export class CommandsViewComponent implements OnInit {
 
   }
   ngOnInit(): void {
+    let id : string | null;
+    id = this.route.snapshot.paramMap.get("id");
+    if(id){
+      this.getCurrentCommandNumber(id);
+    }
+    else{
+      console.log("Hacker man ");
+    }
   }
 
 }
