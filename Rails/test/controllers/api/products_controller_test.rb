e42aad252925960b80481d9ba39637938f62746a
Jérémy Veillette <j.veillette@hotmail.ca>
2021-02-18 21:52:06 -0500

    FUCK!!!!!!!!!!!!!!!!!!!

--- Rails/test/controllers/api/products_controller_test.rb
+++ Rails/test/controllers/api/products_controller_test.rb
@@ -4,4 +4,30 @@ class Api::ProductsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+    test "Can Delete a existing product " do 
+        assert_difference 'Product.count', -1 do
+            delete "/api/products/0"
+        end
+    end
+
+    test "Can't Delete a no existing product " do 
+        assert_raises(ActiveRecord::RecordNotFound) do
+            delete "/api/products/6666"
+        end
+    end
+    test "Can get one products" do
+        get "/api/products/0"
+        assert_response :success
+    end
+    test "can get all products" do
+        get "/api/products/"
+        assert_response :success
+    end
+    test "can't can no existing product " do
+        assert_raises(ActiveRecord::RecordNotFound) do
+            get "/api/products/666"
+        end
+    end
+
 end
