74ae1022d4bab82930b7a121c96ab808c7cc3aa0
Jerebern_VM_DEBIAN <jere.bern@hotmail.com>
2021-02-25 21:47:30 -0500

    delete en backend command_products

--- Rails/app/controllers/api/command_products_controller.rb
+++ Rails/app/controllers/api/command_products_controller.rb
@@ -4,7 +4,13 @@ def index
     @command = Command.find(params[:command_id])
     render json: {command_products: @command.command_products, succes: true}
 end
-
+def destroy
+    if Command.find(params[:command_id]).command_products.find(params[:id]).destroy
+        render json: {succes: true}
+    else
+        render json: {succes: false}
+    end
+end
 private 
 def is_currentUser?
     unless current_user.id == params[:user_id].to_i || current_user.is_admin == true
