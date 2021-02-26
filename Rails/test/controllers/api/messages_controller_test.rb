e885c4dec6aca120c3476c64836cb0b96aed382b
Félix <felixcm1129@hotmail.ca>
2021-02-25 22:58:29 -0500

    tests message et conversation : index, show et create

--- Rails/test/controllers/api/messages_controller_test.rb
+++ Rails/test/controllers/api/messages_controller_test.rb
@@ -1,6 +1,32 @@
 require 'test_helper'
 
 class Api::MessagesControllerTest < ActionDispatch::IntegrationTest
+  #index
+  test "get all messages" do 
+    post "/users/sign_in", params: {user: {email: "admin@jfj.com", password: "123456"}}
+    get "/api/users/1/messages"
+    messages = response.parsed_body
+    assert_equal(6, messages.count)
+    assert_response :success
+  end
+
+  #show
+  test "get one message from user" do
+    post "/users/sign_in", params: {user: {email: "admin@jfj.com", password: "123456"}}
+    get "/api/users/1/messages/2"
+    message = Message.new(response.parsed_body)
+    assert_equal(Message.find(2), message)
+    assert_response :success
+  end
+  
+  test "can't get message if message doesn't exist" do
+    post "/users/sign_in", params: {user: {email: "admin@jfj.com", password: "123456"}}
+    assert_raises(ActiveRecord::RecordNotFound) do
+      get "/api/users/2/messages/7"
+      assert_response :unprocessable_entity
+    end
+  end
+
   #create
   test "can create message" do
     post "/users/sign_in", params: {user: {email: "admin@jfj.com", password: "123456"}}
@@ -9,5 +35,12 @@ class Api::MessagesControllerTest < ActionDispatch::IntegrationTest
       message = Message.new(response.parsed_body)
       assert_equal(Message.last, message)
       assert_response :success
+  end
+
+  test "can't create message without texte" do
+    post "/users/sign_in", params: {user: {email: "admin@jfj.com", password: "123456"}}
+    post "/api/users/3/messages", params: {message: {texte: "", conversation_id: 2, user_id: 3}}
+      assert_equal({"texte"=>["can't be blank"]}, response.parsed_body)
+      assert_response :unprocessable_entity
   end
 end
