737fce3688447c7b06e13bb9083844d09d0011b8
Jérémy Veillette <j.veillette@hotmail.ca>
2021-03-05 01:52:34 -0500

    j4à

--- Rails/app/models/user_commands_summary.rb
+++ Rails/app/models/user_commands_summary.rb
@@ -1,5 +1,5 @@
 class UserCommandsSummary < ApplicationRecord
-    #self.table_name = "user_commands_summary"
+    self.table_name = "user_commands_summary"
     before_validation do
         throw :abort
     end
