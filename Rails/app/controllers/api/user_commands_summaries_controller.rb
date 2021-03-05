93f6e2885350d3a2ad7cf90c7cc6e5fc15b58030
Jerebern_VM_DEBIAN <jere.bern@hotmail.com>
2021-03-05 05:40:42 -0500

    test sur le command summaries

--- Rails/app/controllers/api/user_commands_summaries_controller.rb
+++ Rails/app/controllers/api/user_commands_summaries_controller.rb
@@ -1,12 +1,11 @@
 class Api::UserCommandsSummariesController < ApplicationController
     before_action :admin_required
     def index 
-        # "Un Switch case c'est pas mal plus sexy qu'une orgie de else if " - Jérémy Bernard 05/03/2021 a 3h du matin 
+        # "Un Switch case c'est pas mal plus sexy qu'un lot de else if " - Jérémy Bernard 05/03/2021 a 3h du matin 
        # Reflexion 5h du matin -> les méthodes les plus inspirantes peuvent être les plus décevantes  
         if params[:q].blank? and params[:s].blank?
             render json: { user_commands_summary: UserCommandsSummary.find_by_sql("SELECT * FROM user_commands_summary"), success: true}
         ##end
-        
         # case params[:s]
         # when "EmailUp"
         #     render json: { user_commands_summary: UserCommandsSummary.sort_by(&email), success: true}
--- Rails/test/controllers/api/user_commands_summaries_controller_test.rb
+++ Rails/test/controllers/api/user_commands_summaries_controller_test.rb
@@ -4,4 +4,27 @@ class Api::UserCommandsSummariesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "test can get user commmand summary report for all users " do
+    post "/users/sign_in", params: {user: {email: "admin@jfj.com", password: "123456"}}
+    get "/api/user_commands_summaries/"
+    success = response.parsed_body["success"]
+    assert_equal(true,success)
+  end
+  
+  test "test can't get user commmand summary when not admin  " do
+    post "/users/sign_in", params: {user: {email: "jere.bern@hotmail.com", password: "123456"}}
+    get "/api/user_commands_summaries/"
+    success = response.parsed_body["success"]
+    error = response.parsed_body["error"]
+    assert_equal("not admin",error)
+    assert_equal(false,success)
+  end
+
+  test "test can't get user commmand summary when not signin" do
+    get "/api/user_commands_summaries/"
+    success = response.parsed_body["success"]
+    error = response.parsed_body["error"]
+    assert_equal("not admin",error)
+    assert_equal(false,success)
+  end
 end
