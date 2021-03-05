d930df8519a6f03b3b6d9d9ddc6f831a04e17ecd
Jerebern_VM_DEBIAN <jere.bern@hotmail.com>
2021-03-05 05:42:10 -0500

    ça va tu merger??

--- Rails/test/controllers/api/user_commands_summaries_controller_test.rb
+++ Rails/test/controllers/api/user_commands_summaries_controller_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 
 class Api::UserCommandsSummariesControllerTest < ActionDispatch::IntegrationTest
-<<<<<<< HEAD
   # test "the truth" do
   #   assert true
   # end
@@ -27,12 +26,5 @@ class Api::UserCommandsSummariesControllerTest < ActionDispatch::IntegrationTest
     error = response.parsed_body["error"]
     assert_equal("not admin",error)
     assert_equal(false,success)
-=======
-  test "can get all commands sommary" do
-    post "/users/sign_in", params: {user: {email: "admin@jfj.com", password: "123456"}}
-    get "/api/commands_sommaries/"
-    assert_equal(UserCommandsSommary.all.count, response.parsed_body['user_commands_sommary'].count)
-    assert_response :success
->>>>>>> 6e73a31370ad8dedaadb6cdbdf63d89754d93075
   end
 end
