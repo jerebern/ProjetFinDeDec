f94aa91019d895207105e06896aceba7f4ce717a
Jérémy Veillette <j.veillette@hotmail.ca>
2021-03-04 22:03:54 -0500

    tite correction

--- Rails/app/controllers/api/products_sommaries_controller.rb
+++ Rails/app/controllers/api/products_sommaries_controller.rb
@@ -1,4 +1,5 @@
 class Api::ProductsSommariesController < ApplicationController
+    before_action :admin_required
     def index
         if params[:q].blank?
             render json: { products_sommary: ProductsSommary.find_by_sql("SELECT * FROM products_sommary").as_json, success: true}
