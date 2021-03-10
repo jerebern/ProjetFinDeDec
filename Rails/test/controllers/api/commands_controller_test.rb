2bfbef13e96b058addf334013e52b44942c83680
Jerebern_VM_DEBIAN <jere.bern@hotmail.com>
2021-03-10 00:46:02 -0500

    réparation des test

--- Rails/test/controllers/api/commands_controller_test.rb
+++ Rails/test/controllers/api/commands_controller_test.rb
@@ -6,7 +6,7 @@ class Api::CommandsControllerTest < ActionDispatch::IntegrationTest
   # end
   test "can list commands" do
     post "/users/sign_in", params: {user: {email: "admin@jfj.com", password: "123456"}}
-    get "/api/users/1/commands"
+    get "/api/commands"
     commands = response.parsed_body["commands"]
     success = response.parsed_body["success"]
     assert_equal(3,commands.count)
@@ -14,43 +14,43 @@ class Api::CommandsControllerTest < ActionDispatch::IntegrationTest
   end
   test "can get one command" do
     post "/users/sign_in", params: {user: {email: "admin@jfj.com", password: "123456"}}
-    get "/api/users/1/commands/1"
+    get "/api/commands/1"
    success = response.parsed_body["success"]
     assert_equal(true,success)
   end
   test "can't get command doesnt belong to this user " do
     post "/users/sign_in", params: {user: {email: "jere.bern@hotmail.com", password: "123456"}}
 
-    get "/api/users/1/commands/2"
+    get "/api/commands/2"
     success = response.parsed_body["success"]
     assert_equal(false,success)
 
   end
   test "can't delete command doesnt belong to this user " do
     post "/users/sign_in", params: {user: {email: "jere.bern@hotmail.com", password: "123456"}}
-      delete "/api/users/1/commands/1"
+      delete "/api/commands/1"
       success = response.parsed_body["success"]
       assert_equal(false,success)
 
     end
   test "can delete command " do
       post "/users/sign_in", params: {user: {email: "admin@jfj.com", password: "123456"}}
-        delete "/api/users/1/commands/1"
+        delete "/api/commands/1"
         success = response.parsed_body["success"]
         assert_equal(true,success)
   
     end  
    test "Can search command " do
     post "/users/sign_in", params: {user: {email: "admin@jfj.com", password: "123456"}}
-    get "/api/users/1/commands", params: {q: "En cours"}
+    get "/api/commands", params: {q: "Payé"}
     commands = response.parsed_body["commands"]
-    assert_equal(1,commands.count)
+    assert_equal(2,commands.count)
     success = response.parsed_body["success"]
     assert_equal(true,success)
    end
    test "Send invalid search querry for command should return 0 " do
     post "/users/sign_in", params: {user: {email: "admin@jfj.com", password: "123456"}}
-    get "/api/users/1/commands", params: {q: "ABCDEFEID"}
+    get "/api/commands", params: {q: "ABCDEFEID"}
     commands = response.parsed_body["commands"]
     assert_equal(0,commands.count)
     success = response.parsed_body["success"]
@@ -59,31 +59,26 @@ class Api::CommandsControllerTest < ActionDispatch::IntegrationTest
    
   test "can update one command " do
     post "/users/sign_in", params: {user: {email: "admin@jfj.com", password: "123456"}}
-    patch "/api/users/1/commands/1", params: {command: {shipping_adress: "TEST1233455"}}
+    patch "/api/commands/1", params: {command: {shipping_adress: "TEST1233455"}}
     success = response.parsed_body["success"]
     assert_equal(true,success)
     
   end
   test "can create a estimated command but doesnt save in database " do
     post "/users/sign_in", params: {user: {email: "admin@jfj.com", password: "123456"}}
-    post "/api/users/1/commands", params:{sendCommand: false}
+    post "/api/commands", params:{sendCommand: false}
     success = response.parsed_body["success"]
     assert_equal(true,success)
     
   end
   test "can create a command" do
     post "/users/sign_in", params: {user: {email: "admin@jfj.com", password: "123456"}}
-    post "/api/users/1/commands"
+    post "/api/commands"
     success = response.parsed_body["success"]
     command = Command.new(response.parsed_body["command"])
     assert_equal(true,success)
   end
-  test "Admin can get another user command" do
-    post "/users/sign_in", params: {user: {email: "admin@jfj.com", password: "123456"}}
-    get "/api/users/2/commands"
-    success = response.parsed_body["success"]
-    assert_equal(true,success)
-  end
+
   
 
 end
